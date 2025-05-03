c2c9c19229a1fd75d104b439e619089aa8048021
CookiePawn <cookiepawn@naver.com>
2025-05-03 13:20:41 +0900

    [fix][permission][fix permission]

--- src/navigation/StackNavigation.tsx
+++ src/navigation/StackNavigation.tsx
@@ -1,12 +1,58 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import { Home, Permission } from '@/screens';
+import LinearGradient from 'react-native-linear-gradient';
+import { useLayoutEffect, useState } from 'react';
+import { Platform, StyleSheet } from 'react-native';
+import { RESULTS, PERMISSIONS, check } from 'react-native-permissions';
 
 const Stack = createStackNavigator();
 
 export default function StackNavigation() {
+    const [permissionStatus, setPermissionStatus] = useState<'checking' | 'granted' | 'denied' | 'blocked'>('checking');
+    const [isLoading, setIsLoading] = useState(true);
+
+    useLayoutEffect(() => {
+        checkPermissions();
+    }, []);
+
+    const checkPermissions = async () => {
+        setIsLoading(true);
+        const cameraPermission = Platform.select({
+            ios: PERMISSIONS.IOS.CAMERA,
+            android: PERMISSIONS.ANDROID.CAMERA,
+        });
+
+        if (!cameraPermission) return;
+
+        const cameraResult = await check(cameraPermission);
+
+        if (cameraResult === RESULTS.GRANTED) {
+            setPermissionStatus('granted');
+        } else if (cameraResult === RESULTS.BLOCKED) {
+            setIsLoading(false);
+            setPermissionStatus('blocked');
+        } else {
+            setIsLoading(false);
+            setPermissionStatus('denied');
+        }
+    };
+
+
+    if (isLoading) {
+        return (
+            <LinearGradient
+                colors={['#9A4DD0', '#280061', '#020105']}
+                style={styles.container}
+                start={{ x: 0, y: 0 }}
+                end={{ x: 0, y: 1 }}
+            >
+            </LinearGradient>
+        );
+    }
+
     return (
         <Stack.Navigator
-            initialRouteName="Permission"
+            initialRouteName={permissionStatus === 'granted' ? 'Home' : 'Permission'}
             screenOptions={{
                 headerShown: false,
             }}
@@ -16,3 +62,9 @@ export default function StackNavigation() {
         </Stack.Navigator>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+});
